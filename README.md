b48d421ee2288b1db3e2d16eef0c8b840f2476c1
Minh Duc Le <42980704+dealoux@users.noreply.github.com>
2023-11-07 17:49:21 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Description
+An android application simulating a sport field reservation service
+- Language: [Java](https://docs.oracle.com/en/java/)
+- IDE: [Android Studio](https://developer.android.com/studio)
---
+++ app/src/main/java/ducle/fieldFinder/HomeActivity.java
@@ -0,0 +1,35 @@
+package ducle.fieldFinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Intent intent = getIntent();
+        String username = (String) intent.getExtras().get("username");
+        String password = (String) intent.getExtras().get("password");
+        String text = "Hello " + username + " (" + password + ")";
+        Toast.makeText(HomeActivity.this, text, Toast.LENGTH_LONG).show();
+
+        Button button = (Button) findViewById(R.id.buttonLogout);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(HomeActivity.this, LoginActivity.class);
+                intent1.putExtra("response", "Logged out");
+                setResult(RESULT_OK, intent1);
+                finish();
+            }
+        });
+    }
+}
